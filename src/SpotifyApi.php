75af80d41ebdefe7240177c3e2e57cb9aacd6872
Andres G. Carmona <ing.andres.carmona@gmail.com>
2019-08-13 12:39:53 -0500

    Added wrappers for request access code and tokens to API class. Added me function to get Spotify user information.

--- src/SpotifyApi.php
+++ src/SpotifyApi.php
@@ -3,6 +3,8 @@
     namespace Polaris;
 
     use GuzzleHttp\Client;
+    use Illuminate\Http\RedirectResponse;
+    use Polaris\Exceptions\SpotifyAuthException;
 
     /**
      * Class SpotifyApi
@@ -14,7 +16,7 @@
         /**
          *
          */
-        const API_URL = 'https://api.spotify.com';
+        const API_URL = 'https://api.spotify.com/v1';
 
         /**
          * Holds a references to Guzzle Client class.
@@ -40,5 +42,61 @@
         {
             $this->client        = $client;
             $this->accountClient = $accountClient;
+        }
+
+        /**
+         * Wrapper around account object. Simplifies calls from end user.
+         *
+         * @return RedirectResponse
+         * @throws Exceptions\SpotifyAuthException
+         */
+        public function requestAccessCode(): RedirectResponse
+        {
+            return $this->accountClient->requestAccessCode();
+        }
+
+        /**
+         * Wrapper around account object. Simplifies calls from end user.
+         *
+         * @param $code
+         * @return mixed
+         * @throws Exceptions\SpotifyAuthException
+         */
+        public function requestAccessToken($code)
+        {
+            return $this->accountClient->requestAccessToken($code);
+        }
+
+        /**
+         * Calls Spotify's https://api.spotify.com/v1/me to get user account information.
+         *
+         * @return mixed
+         * @throws SpotifyAuthException
+         */
+        public function me()
+        {
+            // Try to get access token first from memory and then from session.
+            $accessToken = $this->accountClient->getAccessToken() ?? session('spotify_session')->access_token;
+
+            // If no access token found, then raise exception.
+            if (!$accessToken) {
+                throw new SpotifyAuthException('Invalid access token provided.');
+            }
+
+            // Return json decode response.
+            return $this->json(
+                $this->client->get(self::API_URL.'/me', $this->accountClient->getAuthHeaders())
+            );
+        }
+
+        /**
+         * Json encode response param.
+         *
+         * @param $response
+         * @return mixed
+         */
+        public function json($response)
+        {
+            return json_decode($response, false);
         }
     }
