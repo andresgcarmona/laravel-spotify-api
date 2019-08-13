9bdb00d0e1b08f4f8a426631e6225ffc6bd6e491
Andres G. Carmona <ing.andres.carmona@gmail.com>
2019-08-13 16:12:23 -0500

    Added code that validates the access token based in the expire_in field. If the access token is invalid It is refreshed with a call to refreshAccessToken.

--- src/SpotifyAccount.php
+++ src/SpotifyAccount.php
@@ -2,11 +2,16 @@
 
     namespace Polaris;
 
+    use Carbon\Carbon;
     use GuzzleHttp\Client;
     use GuzzleHttp\Exception\RequestException;
     use Illuminate\Http\RedirectResponse;
+    use Illuminate\Session\SessionManager;
+    use Illuminate\Session\Store;
     use Illuminate\Support\Str;
     use Polaris\Exceptions\SpotifyAuthException;
+    use Psr\Http\Message\ResponseInterface;
+    use stdClass;
 
     /**
      *
@@ -17,6 +22,7 @@
     class SpotifyAccount
     {
         protected const ACCOUNT_URL  = 'https://accounts.spotify.com';
+        protected const SESSION_NAME = 'spotify_session';
 
         /**
          * Holds a references to Guzzle client object.
@@ -65,7 +71,7 @@
          *
          * @var int
          */
-        protected $expirationTime;
+        protected $expiresAt;
 
         /**
          * Array of scopes that were granted by the user.
@@ -91,13 +97,23 @@
         }
 
         /**
+         * Return the object saved in Session under SESSION_NAME key.
+         *
+         * @return SessionManager|Store|mixed
+         */
+        public function getAccessTokenSession()
+        {
+            return session(self::SESSION_NAME, null);
+        }
+
+        /**
          * Returns access token.
          *
          * @return string
          */
-        public function getAccessToken(): string
+        public function getAccessToken(): ?string
         {
-            return $this->accessToken;
+            return $this->accessToken ?? session(self::SESSION_NAME) ? session(self::SESSION_NAME)->access_token : null;
         }
 
         /**
@@ -107,7 +123,7 @@
          */
         public function getRefreshToken(): string
         {
-            return $this->refreshToken;
+            return $this->refreshToken ?? session(self::SESSION_NAME) ? session(self::SESSION_NAME)->refresh_token : null;
         }
 
         /**
@@ -115,9 +131,9 @@
          *
          * @return int
          */
-        public function getExpirationTime(): string
+        public function getExpiresAt(): string
         {
-            return $this->expirationTime;
+            return $this->expiresAt;
         }
 
         /**
@@ -189,13 +205,7 @@
                 // Encode response as json object.
                 $response = json_decode($response->getBody(), false);
 
-                // Set local state fields.
-                if (isset($response->access_token, $response->refresh_token)) {
-                    $this->accessToken    = $response->access_token;
-                    $this->refreshToken   = $response->refresh_token;
-                    $this->expirationTime = $response->expires_in;
-                    $this->grantedScopes  = $response->scopes ?? [];
-                }
+                $this->updateSession($response);
 
                 return $response;
             } catch (RequestException $exception) {
@@ -204,13 +214,105 @@
         }
 
         /**
+         * Calls /api/token with a grant_type of refresh_token to update de access token.
+         *
+         * @return mixed|ResponseInterface
+         * @throws SpotifyAuthException
+         */
+        public function refreshAccessToken()
+        {
+            // Get access token from memory or from session.
+            $refreshToken = $this->refreshToken ?? $this->getAccessTokenSession()->refresh_token;
+
+            // If refresh token is null or not isset throw an exception.
+            if (!$refreshToken) {
+                throw new SpotifyAuthException('Invalid refresh access token.', 402);
+            }
+
+            try {
+                // Send post request to api/token end point to refresh the access token.
+                $response = $this->client->post(self::ACCOUNT_URL.'/api/token', [
+                    'headers'     => [
+                        'Authorization' => 'Basic '.base64_encode($this->clientId.':'.$this->clientSecret),
+                        'Accept'        => 'application/json',
+                    ],
+                    'form_params' => [
+                        'grant_type'    => 'refresh_token',
+                        'refresh_token' => $refreshToken,
+                    ],
+                ]);
+
+                // Encode response as json object.
+                $response = json_decode($response->getBody(), false);
+
+                $this->updateSession($response);
+
+                return $response;
+            } catch (RequestException $exception) {
+                throw new SpotifyAuthException($exception->getMessage(), $exception->getCode());
+            }
+        }
+
+        /**
+         * Returns true if the accessToken is set AND the expiration time (expires_at) is less than or equal to now().
+         *
+         * @return bool
+         */
+        public function isAccessTokenValid(): bool
+        {
+            // getAccessToken returns the accessTokenSession field in this class if present, or the access_token from session.
+            $accessTokenSession = $this->getAccessTokenSession();
+
+            return $accessTokenSession && Carbon::createFromTimestamp($accessTokenSession->expires_at)
+                                                ->lte(Carbon::now());
+        }
+
+        /**
+         * Validate the access token. If the token is not valid then calls refreshAccessToken to get a new one.
+         *
+         * @return bool
+         * @throws SpotifyAuthException
+         */
+        public function validateAccessToken(): bool
+        {
+            // Token is null or has expired.
+            if (!$this->isAccessTokenValid()) {
+                $this->refreshAccessToken();
+            }
+
+            return true;
+        }
+
+        /**
          * Returns the formatted URL to request the access code.
          *
          * @param  array  $params
          * @return string
          */
-        private function getRequestAccessCodeUrl(array $params): string
+        protected function getRequestAccessCodeUrl(array $params): string
         {
             return self::ACCOUNT_URL.'/authorize?'.http_build_query($params);
+        }
+
+        /**
+         * Updates the session object with fresh data.
+         *
+         * @param  stdClass  $response
+         */
+        protected function updateSession(stdClass $response): void
+        {
+            // Set expires_at field.
+            $response->expires_at = Carbon::now()->addSeconds($response->expires_in)->timestamp;
+
+            // Set local state fields.
+            if (isset($response->access_token)) {
+                $this->accessToken   = $response->access_token;
+                $this->refreshToken  = $response->refresh_token ?? $this->getRefreshToken();
+                $this->expiresAt     = $response->expires_at;
+                $this->grantedScopes = $response->scopes ?? [];
+            }
+
+            // Save response in session.
+            session(self::SESSION_NAME, $response);
         }
     }
